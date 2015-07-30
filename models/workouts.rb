b49456f3f970541bd2acf603f6c1f1db64aac9e9
lupetyman <lupetyman@gmail.com>
2015-07-30 03:11:56 +0000

    Workout randomization based on selection

--- models/workouts.rb
+++ models/workouts.rb
@@ -1,12 +1,18 @@
-workouts = {
-  :chest => ["Bench Press", "Incline Press", "Dumbbell Press", "Pushups"],
-  :triceps => ["Skullcrushers", "Pull Downs"],
-  :biceps => ["Dumbell Curls", "Bar Curls" ],
-  :legs => ["Squats", "Leg Press"],
-  :shoulders => ["Overhead Press", ""],
-  :back => ["Shrugs", "One Arm Pulls"],
-  :cardio => ["Treadmill"]
+$workouts = {
+  "chest" => ["Bench Press", "Incline Press", "Dumbbell Press", "Pushups"],
+  "triceps" => ["Skullcrushers", "Pull Downs"],
+  "biceps" => ["Dumbell Curls", "Bar Curls" ],
+  "legs" => ["Squats", "Leg Press", "Calves"],
+  "shoulders" => ["Overhead Press"],
+  "back" => ["Shrugs", "One Arm Pulls"],
+  "abs" => ["Crunches", "Planks", "Ab Wheel"],
+  "cardio" => ["Treadmill"]
   }
 
-
-workouts[:chest].sample
+def randomize(hash)
+  @neworkout = []
+  hash.each_value do | muscle |
+    @neworkout.push($workouts[muscle].sample)
+  end
+  return @neworkout
+end
